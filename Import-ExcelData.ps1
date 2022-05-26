500fdfb3e5db7d583426943f764a223869d0b1a9
sait-ahall <andrew.hall@sait.ca>
2022-05-26 13:28:45 -0600

    added script to read sechedule file and generate onetoone files

---
+++ Import-ExcelData.ps1
@@ -0,0 +1,81 @@
+#use the import excel module to read in the sheet
+$exData = Import-Excel 'C:\Users\ahall\Southern Alberta Institute of Technology\Campus Network Upgrade Project - General\OCM\Schedule\Burns Switch Upgrade schedule.xlsx'
+
+#Create a new object to finessse the switch and room data
+
+$objOut = @()
+
+# set the order value to generate the specific switch configs:
+# make sure that the switch configs are backed up on the switches and are downloaed to Y:
+$exdataSelected = $exdata | Where-Object order -in (3,2)
+foreach ($row in $exdataSelected) {
+
+    if ($row.DeviceName) {
+        $x = New-Object -TypeName psobject -Property @{
+            RoomID         = $row.RoomID
+            ConfigFileName = 'cisco' + $row.DeviceName + '-confg-dump'
+            DeviceName     = 'cisco' + $row.DeviceName
+            Order          = [int]$row.order
+            UGWeek         = $row.'Planned upgrade week'
+            sshCommand     = 'ssh ' + 'cisco' + $row.DeviceName
+            BackupCommand  = 'copy running-config tftp://10.194.133.10/cisco' + $row.DeviceName + '-confg-dump'
+        }
+
+        $objOut += $x #>
+    }
+}
+
+
+$objOutSorted = ($objOut | Sort-Object order)
+
+# create command file for cut and paste
+"" | Out-File -Path 'c:\foo\backupcmds.txt'
+
+foreach ($row in $objOutSorted) {
+    $row.UGWeek | Out-File -Path 'c:\foo\backupcmds.txt' -Append
+    $row.sshCommand | Out-File -Path 'c:\foo\backupcmds.txt' -Append
+    #"wr" | Out-File -Path 'c:\foo\backupcmds.txt' -Append
+    $row.BackupCommand | Out-File -Path 'c:\foo\backupcmds.txt' -Append
+    "exit`n`n" | Out-File -Path 'c:\foo\backupcmds.txt' -Append
+
+}
+
+#create input csv for roomlist, set vars such as output folder for the Convert-CiscoToAruba module
+$objOutRooms = @()
+
+foreach ($row in $objOutSorted) {
+    $x = New-Object -TypeName psobject -Property @{
+        CiscoConfigFilePath = "y:\backups\"+ $row.RoomID +'\'+ $row.ConfigFileName
+        OutputDir = 'c:\foo\out1\'+ $row.RoomID
+        ConversionMethod = 'OneToOne'
+        ConsoleVisualizer ='False'
+    }
+    $objOutRooms += $x
+}
+
+
+$Pline = @()
+foreach ($row in $objOutRooms) {
+
+    #convert the text to a bool for the visualiser flag
+    if ($row.ConsoleVisualizer.toupper() -eq 'FALSE') {
+        $ShowVis = $false
+    } else {
+        $ShowVis = $true
+    }
+
+    $x = New-Object -TypeName psobject -Property @{
+        CiscoConfigFilePath = $row.CiscoConfigFilePath
+        OutputDir           = $row.OutputDir
+        ConversionMethod    = $row.ConversionMethod
+        ConsoleVisualizer   = $ShowVis
+    }
+
+    $pline += $x
+}
+
+$pline | Convert-CiscoToAruba
+
+
+
+
