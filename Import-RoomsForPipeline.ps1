f8887ce86500becd410f87212f88b2c4c821c632
sait-ahall <andrew.hall@sait.ca>
2022-05-26 10:43:32 -0600

    added import rooms

---
+++ Import-RoomsForPipeline.ps1
@@ -0,0 +1,27 @@
+# import the rooms for the pipline
+
+
+$roomListCSV = 'C:\foo\roomlist.csv'
+$RoomList = Import-CSV -path $roomListCSV
+
+$Pline = @()
+foreach ($row in $RoomList) {
+
+    #convert the text to a bool for the visualiser flag
+    if ($row.ConsoleVisualizer.toupper() -eq 'FALSE') {
+        $ShowVis = $false
+    } else {
+        $ShowVis = $true
+    }
+
+    $x = New-Object -TypeName psobject -Property @{
+        CiscoConfigFilePath = $row.CiscoConfigFilePath
+        OutputDir           = $row.OutputDir
+        ConversionMethod    = $row.ConversionMethod
+        ConsoleVisualizer   = $ShowVis
+    }
+
+    $pline += $x
+}
+
+$pline | Convert-CiscoToAruba
